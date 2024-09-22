6113c13438ab700b80760c16d0d069a668a3e237
Sykzes <74907431+Sykzes@users.noreply.github.com>
2024-09-23 00:01:38 +0200

    Tidied project a bit.

--- shaders.cmake
+++ shaders.cmake
@@ -4,7 +4,7 @@ function(add_shader FILE TARGET HEADER VARNAME)
     add_custom_command(
         OUTPUT ${CMAKE_SOURCE_DIR}/${HEADER}
         MAIN_DEPENDENCY ${CMAKE_SOURCE_DIR}/${FILE}
-        DEPENDS ${CMAKE_SOURCE_DIR}/Common.hlsl
+        DEPENDS ${CMAKE_SOURCE_DIR}/shaders/Common.hlsl
         COMMAND ${DXC} /T ${TARGET} /Fh ${CMAKE_SOURCE_DIR}/${HEADER} /Vn ${VARNAME} ${CMAKE_SOURCE_DIR}/${FILE}
         VERBATIM
     )
